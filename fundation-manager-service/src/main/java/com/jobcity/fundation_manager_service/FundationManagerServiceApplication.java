def8135fed65b8b971093c95729ab94408032c94
Jlazarte <joselazartelagos@gmail.com>
2024-07-05 02:26:01 -0300

    [BE] Create backend

---
+++ fundation-manager-service/src/main/java/com/jobcity/fundation_manager_service/FundationManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jobcity.fundation_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundationManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundationManagerServiceApplication.class, args);
+	}
+
+}
---
+++ fundation-manager-service/src/test/java/com/jobcity/fundation_manager_service/FundationManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobcity.fundation_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundationManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
